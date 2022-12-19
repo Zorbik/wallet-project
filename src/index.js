9b296af01390dbc65eba998002694ef209fbd88d
Andrii-Hr <andriihryb@ukr.net>
2022-12-19 17:40:02 +0200

    fix deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-import './common/main.scss';
+// import './common/main.scss';
 // import './index.css';
 // import { App } from './App';
 // import { ThemeProvider } from 'styled-components';
