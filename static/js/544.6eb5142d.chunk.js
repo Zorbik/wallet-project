3a962ecf279f20eecf5b73b19457a54046aff4bc
Zorbik <37942049+Zorbik@users.noreply.github.com>
2022-12-22 09:42:52 +0000

    Deploying to gh-pages from @ Zorbik/wallet-project@6eb058724337cff8a29febd9e0fac083c1695cf7 🚀

---
+++ static/js/544.6eb5142d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwallet_project=self.webpackChunkwallet_project||[]).push([[544],{544:function(e,t,c){c.r(t);var l=c(329);t.default=function(){return(0,l.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=544.6eb5142d.chunk.js.map
