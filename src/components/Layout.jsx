95922d99cfe590566d355be8a7c83e6317b1d5c4
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2022-12-22 14:13:25 +0200

    add Container

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Box } from '../components/Box';
-import styled from 'styled-components';
 import { Header } from './header/header';
 import { AppBar } from './appBar/AppBar';
+import { Container } from './appBar/AppBar.styled';
 
 export const Layout = () => {
   return (
@@ -18,18 +18,3 @@ export const Layout = () => {
     </Container>
   );
 };
-
-const Container = styled(Box)`
-  box-sizing: border-box;
-  padding: 0 16px;
-  margin: 0 auto;
-  @media screen and (min-width: 480px) {
-    width: 480px;
-  }
-  @media screen and (min-width: 768px) {
-    width: 768px;
-  }
-  @media screen and (min-width: 1280px) {
-    width: 1280px;
-  }
-`;
--- src/pages/registrationPage/registrationPage.js
+++ src/pages/registrationPage/registrationPage.js
@@ -1,4 +1,5 @@
-import RegisterForm from "../../components/registrationForm/registrationForm";
+import { Container } from '../../components/appBar/AppBar.styled';
+import RegisterForm from '../../components/registrationForm/registrationForm';
 import Group from '../../images/Group.svg';
 import {
   RegistrationPageImg,
@@ -8,15 +9,17 @@ import {
 
 const RegistrationPage = () => {
   return (
-    <RegistrationPageStyled>
-      <RegistrationPageImg>
-        <img src={Group} alt="" width="436" />
-        <h1> Finance App </h1>
-      </RegistrationPageImg>
-      <RegisterPageForm>
-        <RegisterForm />
-      </RegisterPageForm>
-    </RegistrationPageStyled>
+    <Container>
+      <RegistrationPageStyled>
+        <RegistrationPageImg>
+          <img src={Group} alt="" width="436" />
+          <h1> Finance App </h1>
+        </RegistrationPageImg>
+        <RegisterPageForm>
+          <RegisterForm />
+        </RegisterPageForm>
+      </RegistrationPageStyled>
+    </Container>
   );
 };
 export default RegistrationPage;
