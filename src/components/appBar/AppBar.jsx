431bcc0de6dc665b124008211e3a2cda35448b24
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-12-24 15:07:09 +0200

    fixed adaptive version app

--- src/components/appBar/AppBar.jsx
+++ src/components/appBar/AppBar.jsx
@@ -1,5 +1,14 @@
-import { Box } from '../Box';
-import { NavMenu, Link, TotalBalanceWrapper, Title } from './AppBar.styled';
+import { useMedia } from 'react-use';
+import {
+  NavMenu,
+  Link,
+  TotalBalanceWrapper,
+  Title,
+  BoxStyled,
+  BoxWrapper,
+  BoxStyledModal,
+  NavMenuModal,
+} from './AppBar.styled';
 
 import { ReactComponent as HomeIcon } from '../../icons/HomeIcon.svg';
 import { ReactComponent as DiagramIcon } from '../../icons/statIcon.svg';
@@ -10,26 +19,51 @@ import { Currency } from '../currency/currency';
 
 export const AppBar = () => {
   const { balance } = useSelector(state => state.userData.user);
+  const isWide = useMedia('(max-width:768px)');
 
   return (
-    <Box>
-      <Box display="flex" flexDirection="column">
-        <NavMenu>
-          <Link to="/">
-            <HomeIcon />
-            <span>Home</span>
-          </Link>
-          <Link to="statistic">
-            <DiagramIcon />
-            <span>Statistics</span>
-          </Link>
-        </NavMenu>
-        <TotalBalanceWrapper>
-          <Title>Your balance</Title>
-          <Balance balance={balance} />
-        </TotalBalanceWrapper>
-      </Box>
-      <Currency />
-    </Box>
+    <>
+      {isWide ? (
+        <BoxWrapper>
+          <BoxStyledModal>
+            <NavMenuModal>
+              <Link to="/">
+                <HomeIcon />
+              </Link>
+              <Link to="statistic">
+                <DiagramIcon />
+              </Link>
+              <Link to="currency">
+                <DiagramIcon />
+              </Link>
+            </NavMenuModal>
+            <TotalBalanceWrapper>
+              <Title>Your balance</Title>
+              <Balance balance={balance} />
+            </TotalBalanceWrapper>
+          </BoxStyledModal>
+        </BoxWrapper>
+      ) : (
+        <BoxWrapper>
+          <BoxStyled>
+            <NavMenu>
+              <Link to="/">
+                <HomeIcon />
+                <span>Home</span>
+              </Link>
+              <Link to="statistic">
+                <DiagramIcon />
+                <span>Statistics</span>
+              </Link>
+            </NavMenu>
+            <TotalBalanceWrapper>
+              <Title>Your balance</Title>
+              <Balance balance={balance} />
+            </TotalBalanceWrapper>
+          </BoxStyled>
+          <Currency />
+        </BoxWrapper>
+      )}
+    </>
   );
 };
